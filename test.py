8fde06b94112a85b2668455a6dd18ed663dec261
Niks <42009476+cliquant@users.noreply.github.com>
2024-12-12 00:03:11 +0200

    shadcn install 2

--- test.py
+++
@@ -1,6 +0,0 @@
-async def notingroup(userid):
-    check_member = await main.tbot.get_chat_member(-1002125422822, userid)
-    if check_member.status not in ["member", "creator", "administrator", "owner"]:
-        return True
-    else:
-        return False
